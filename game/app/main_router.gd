36f8e5735e6e73be06ea186aadc85ac2c1c79e73
ws951125 <ws951125@gmail.com>
2026-09-12 17:58:13 +0800

    feat: support in-app creator training mode switching

--- game/app/main_router.gd
+++ game/app/main_router.gd
@@ -4,14 +4,27 @@ const TRAINING_SCENE = preload("res://game/runtime/main.tscn")
 const CREATOR_SCENE = preload("res://game/creator/creator_studio.tscn")
 
 var app_mode := "training"
+var active_instance: Node
 
 func _ready() -> void:
 	app_mode = _requested_mode()
-	var selected_scene: PackedScene = CREATOR_SCENE if app_mode == "creator" else TRAINING_SCENE
-	var instance := selected_scene.instantiate()
-	add_child(instance)
-	if instance is Control:
-		instance.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	_mount_mode(app_mode)
+
+func switch_mode(requested_mode: String) -> void:
+	var normalized_mode := "creator" if requested_mode.strip_edges().to_lower() == "creator" else "training"
+	if active_instance != null and is_instance_valid(active_instance):
+		remove_child(active_instance)
+		active_instance.queue_free()
+	active_instance = null
+	app_mode = normalized_mode
+	_mount_mode(app_mode)
+
+func _mount_mode(mode_name: String) -> void:
+	var selected_scene: PackedScene = CREATOR_SCENE if mode_name == "creator" else TRAINING_SCENE
+	active_instance = selected_scene.instantiate()
+	add_child(active_instance)
+	if active_instance is Control:
+		active_instance.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
 	_set_web_state()
 
 func _requested_mode() -> String:
